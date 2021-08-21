d3b231f9d8c5045e08a1c7a5e2aa783f36923e57
Luis_velarde15@outlook.com <Luis_velarde15@outlook.com>
2021-08-20 18:05:35 -0600

    Agregar archivos de proyecto.

---
+++ Helpers/Datos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiEmpleado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiEmpleado.Helpers
+{
+    public class Datos : DbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; } //Tabla categoria de la base de datos
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string Conexion = @"Server = surtidorvelarde.database.windows.net;
+                                Database = surtidorvelardedb; 
+                                User = admindb; 
+                                Password =luis1598*;";
+            /*@"Server = surtidor.database.windows.net;
+               Database = surtidordb;
+               User = admindb;
+               Password = 12AB34cd*;";*/ // Servidor Maestro Beltran
+            optionsBuilder.UseSqlServer(Conexion);
+        }
+    }
+}
